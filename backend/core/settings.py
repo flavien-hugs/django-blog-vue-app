d269ad5e02fd42963ae64ebbc1095a8eefb31d92
flavien-hugs <flavienhugs@pm.me>
2022-01-09 18:57:35 +0000

    improve settings

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -184,6 +184,18 @@ DISALLOWED_USER_AGENTS = [
     re.compile(r'^sohu-search'),
 ]
 
+# https://docs.djangoproject.com/en/dev/ref/settings/#session-cookie-httponly
+SESSION_COOKIE_HTTPONLY = True
+
+# https://docs.djangoproject.com/en/dev/ref/settings/#csrf-cookie-httponly
+CSRF_COOKIE_HTTPONLY = True
+
+# https://docs.djangoproject.com/en/dev/ref/settings/#secure-browser-xss-filter
+SECURE_BROWSER_XSS_FILTER = True
+
+# https://docs.djangoproject.com/en/dev/ref/settings/#x-frame-options
+X_FRAME_OPTIONS = "SAMEORIGIN"
+
 # Show summernote with Bootstrap4
 
 SUMMERNOTE_THEME = 'bs4'
